acf29c665ca4777b93d6324a9e0f5a920ff50269
ryan <rripken3@gatech.edu>
2022-10-26 09:09:12 -0700

    Added call to copy dependencies.

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 @echo on
 
+
+
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_181
 
 set PROJ_DIR=.\
@@ -8,12 +10,13 @@ set CP=%PROJ_DIR%\target\*;%PROJ_DIR%\src\main\resources
 
 set JVM_OPTS=
 set JVM_OPTS=%JVM_OPTS% -Dmetrics-enabled=true
-set JVM_OPTS=%JVM_OPTS% -Dcwms.dbi.ConnectUsingUrl="http://localhost:7000/swt-data/"
-set JVM_OPTS=%JVM_OPTS% -DCWMS_HOME="%PROJ_DIR%\src\test"
+set JVM_OPTS=%JVM_OPTS% -DCWMS_HOME=%PROJ_DIR%
 set JVM_OPTS=%JVM_OPTS% -Djava.util.logging.config.file=logging.properties
 
 set MAIN_CLASS=org.python.util.jython
 set JYTHON_SCRIPT=%PROJ_DIR%\src\main\resources\NWD_Reservoir_Bulletin_1Day.py
+
+call mvn dependency:copy-dependencies
 
 "%JAVA_HOME%\bin\java.exe" %JVM_OPTS% -cp "%CP%" %MAIN_CLASS% %JYTHON_SCRIPT%
 
