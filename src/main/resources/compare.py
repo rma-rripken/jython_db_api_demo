91a1ba51c99fd10661109d479b03c74802a53337
ryan <ryan@rmanet.com>
2022-11-04 12:49:40 -0700

    renamed variables and methods to be more pythonic

--- src/main/resources/compare.py
+++ src/main/resources/compare.py
@@ -14,7 +14,7 @@ import sys, os
 import RADARAPI
 import DBAPI
 
-def readListFromFile(filename):
+def read_list(filename):
 	retval = []
 	with open(filename, 'r') as f:
 		data = f.read()
@@ -22,12 +22,12 @@ def readListFromFile(filename):
 			retval = data.split("\n")
 	return retval
 
-def writeListToFile(filename, list):
+def write_list(filename, list):
 	with open(filename, 'w') as f:
 		for item in list:
 			f.writelines(item + '\n')
 
-def perform(input_dict, CwmsDb):
+def perform(input_dict, cwms_db):
 	retval = {}
 	retval['method'] = input_dict['method']
 	path = input_dict["ts"]
@@ -35,21 +35,21 @@ def perform(input_dict, CwmsDb):
 	retval["office"] = input_dict["office"]
 	
 	if input_dict['timezone'] is not None:
-		CwmsDb.setTimeZone(input_dict['timezone'])
+		cwms_db.setTimeZone(input_dict['timezone'])
 	else:
-		CwmsDb.setTimeZone('US/Central')
+		cwms_db.setTimeZone('US/Central')
 		
-	retval['timezone'] = CwmsDb.getTimeZoneName()
+	retval['timezone'] = cwms_db.getTimeZoneName()
 	
 	if input_dict['method'] == "get":
 		start = input_dict["start"]
 		end = input_dict["end"]
-		response = CwmsDb.get(path, start, end)
+		response = cwms_db.get(path, start, end)
 		
 	elif input_dict['method'] == "read":
 		start = ""
 		end = ""
-		response = CwmsDb.read(path)
+		response = cwms_db.read(path)
 
 	retval["start"] = start
 	retval["end"] = end
@@ -91,53 +91,50 @@ def capture(infile, outfile, use_radar, radar_url, cache_pathnames):
 	input_dicts = [row for row in input_reader]
 	first_dict = input_dicts[0]
 	
-	pathnames = [row['ts'] for row in input_dicts]
+	used_ts = [row['ts'] for row in input_dicts]
 	
-	
-	StartTwStr = first_dict['start']
-	EndTwStr = first_dict['end']
+	start_tw_str = first_dict['start']
+	end_tw_str = first_dict['end']
 	office = first_dict['office']
 	tz = first_dict['timezone']
 	
 	if use_radar:
-		CwmsDb = RADARAPI.open(url=radar_url, office=office)
-		CwmsDb.setTimeZone(tz)
-		CwmsDb.setTimeWindow(StartTwStr, EndTwStr)
-		CwmsDb.setTrimMissing(False)
+		cwms_db = RADARAPI.open(url=radar_url, office=office)
+		cwms_db.setTimeZone(tz)
+		cwms_db.setTimeWindow(start_tw_str, end_tw_str)
+		cwms_db.setTrimMissing(False)
 	else:
 		# This uses the old DBAPI and references values in config/properties
-		CwmsDb = DBAPI.open()
-		CwmsDb.setOfficeId(office)
-		CwmsDb.setTimeZone(tz)
-		CwmsDb.setTimeWindow(StartTwStr, EndTwStr)
-		CwmsDb.setTrimMissing(False)
+		cwms_db = DBAPI.open()
+		cwms_db.setOfficeId(office)
+		cwms_db.setTimeZone(tz)
+		cwms_db.setTimeWindow(start_tw_str, end_tw_str)
+		cwms_db.setTrimMissing(False)
 
 	# The pathname fetch can be slow so if repeatedly running this script
 	# it can be useful to save the list of pathnames to a file.
 	if cache_pathnames:
-		path_cache = "PathnameList-%s.txt" % (office)
+		path_cache = "pathnames-%s.txt" % (office)
 		if os.path.exists(path_cache):
-			PathnameList = readListFromFile(path_cache)
+			pathnames = read_list(path_cache)
 		else:
 			# Get list of pathnames in database
-			PathnameList = CwmsDb.getPathnameList()
-			if PathnameList is not None and len(PathnameList) > 0:
-				writeListToFile(path_cache, PathnameList)
+			pathnames = cwms_db.getPathnameList()
+			if pathnames is not None and len(pathnames) > 0:
+				write_list(path_cache, pathnames)
 	else:
-		PathnameList = CwmsDb.getPathnameList()
+		pathnames = cwms_db.getPathnameList()
 	
-	logger.info('Got PathnameList length:%s' % (len(PathnameList)))
+	logger.info('Got pathnames length:%s' % (len(pathnames)))
 	
-	needsToHave = set(pathnames)
-	missing = needsToHave.difference(set(PathnameList))
-	
+	missing = set(used_ts).difference(set(pathnames))
 	if len(missing) > 0:
 		logger.warning("Missing required pathnames: %s" % (missing))
 	
 	output_writer = csv.writer(outfile)
 	output_writer.writerow(input_reader.fieldnames)
 	for row in input_dicts:
-		d = perform(row, CwmsDb)
+		d = perform(row, cwms_db)
 		output_writer.writerow([d[k] for k in input_reader.fieldnames])
 		warn_about_differences(row, d)
 	
