275d71ed9b54c870c5640ed5701f94f9072d34e8
ryan <ryan@rmanet.com>
2022-11-04 12:23:29 -0700

    Supporting files for test script

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_181
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202
 
 set PROJ_DIR=.\
 
@@ -14,10 +14,11 @@ set JVM_OPTS=%JVM_OPTS% -DCWMS_HOME=%PROJ_DIR%
 set JVM_OPTS=%JVM_OPTS% -Djava.util.logging.config.file=logging.properties
 
 set MAIN_CLASS=org.python.util.jython
-set JYTHON_SCRIPT=%PROJ_DIR%\src\main\resources\NWD_Reservoir_Bulletin_1Day.py
+set JYTHON_SCRIPT=%PROJ_DIR%src\main\resources\compare.py
 
-call mvn dependency:copy-dependencies
 
-"%JAVA_HOME%\bin\java.exe" %JVM_OPTS% -cp "%CP%" %MAIN_CLASS% %JYTHON_SCRIPT%
+call "C:\Program Files\JetBrains\IntelliJ IDEA 2021.2.2\plugins\maven\lib\maven3\bin\mvn" dependency:copy-dependencies
+
+"%JAVA_HOME%\bin\java.exe" %JVM_OPTS% -cp "%CP%" %MAIN_CLASS% %JYTHON_SCRIPT% --radar-url=http://localhost:7000/swt-data/
 
 rem > %temp%\out.log 2>&1
